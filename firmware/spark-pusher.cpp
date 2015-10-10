97c1530fd442ea853b3de83c74cbb51a02da4c82
TheShalit <shalevshalit@gmail.com>
2015-10-10 23:18:53 +0300

    add setup

--- firmware/spark-pusher.cpp
+++ firmware/spark-pusher.cpp
@@ -1,3 +1,19 @@
 #include <spark-pusher.h>
 #include <Ethernet.h>
 #include <PusherClient.h>
+
+PusherClient client;
+
+void setup() {
+  if(client.connect("your-api-key-here")) {
+    client.subscribe("robot_channel");
+    client.bind("forward", moveForward);
+  }
+  else {
+    while(1) {}
+  }
+}
+
+void loop() {
+
+}
